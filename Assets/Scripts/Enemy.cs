5bd3671b7d65816a599cbeee6b094e77c88f6e17
sonac.kim <104493771+kimsonac@users.noreply.github.com>
2022-11-06 21:29:54 +0900

    [Add] NavMeshAgent 추가 및 이동 경로 추가

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    
+    [SerializeField] private Transform startPosition;
+    [SerializeField] private Transform[] destinations;
+    private NavMeshAgent agent;
+    private Vector3 next;
+    private int index;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        transform.position = startPosition.position;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(_MoveToDestination());
+    }
+
+    private IEnumerator _MoveToDestination()
+    {
+        while(index < destinations.Length)
+        {
+            yield return null;
+            next = new Vector3(destinations[index].position.x, 0, destinations[index].position.z);
+            agent.SetDestination(destinations[index].position);
+
+            if(agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if(!agent.hasPath || agent.velocity.sqrMagnitude == 0)
+                {
+                    index++;
+                }
+            }
+        }
+    }
 }
